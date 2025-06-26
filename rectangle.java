0a4781f11f190eb4b5ce41919efa7b0e3259e208
Sharvil Mulay <sharvilmulay@gmail.com>
2025-06-26 12:26:20 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,12 @@
+public class rectangle {
+    public static void main(String[] args) {
+        for(int i=0;i<=4;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        
+    }
+    
+}
